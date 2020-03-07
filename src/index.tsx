0d6fbf2247c2bf01695a54a709c7fb94ff9a5246
José Gomes <josepgomes@ua.pt>
2020-03-07 16:10:00 +0000

    0.1.0

--- src/index.tsx
+++ src/index.tsx
@@ -4,26 +4,26 @@ import { RefookHooks, RefookConfiguratorProps, RefookRefs } from "./types";
 const refs: RefookRefs = {};
 
 export default ({ hooks }: { hooks: RefookHooks }) => {
-    const names = Object.keys(hooks);
+  const names = Object.keys(hooks);
 
-    const InnerConfigurator = (props: RefookConfiguratorProps) => {
-        props.setHookRef(props.hookName, hooks[props.hookName]());
-        return null;
-    };
+  const InnerConfigurator = (props: RefookConfiguratorProps) => {
+    props.setHookRef(props.hookName, hooks[props.hookName]());
+    return null;
+  };
 
-    const setHookRef = (hookName: string, hookRef: () => void) => {
-        refs[hookName] = hookRef;
-    };
+  const setHookRef = (hookName: string, hookRef: () => void) => {
+    refs[hookName] = hookRef;
+  };
 
-    return (
-        <>
-            {names.map((name, index) => (
-                <InnerConfigurator key={index} hookName={name} setHookRef={setHookRef} />
-            ))}
-        </>
-    );
+  return (
+    <>
+      {names.map((name, index) => (
+        <InnerConfigurator key={index} hookName={name} setHookRef={setHookRef} />
+      ))}
+    </>
+  );
 };
 
 export const use = () => {
-    return refs[name];
-}
+  return refs[name];
+};
